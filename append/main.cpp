d2d99c04001e0f9f5e4842e712542373ce6a40f5
Kaye <kaaboaye@gmail.com>
2017-04-27 17:29:01 +0200

    Version in usage

--- append/main.cpp
+++ append/main.cpp
@@ -37,7 +37,7 @@ int main(int argc, char *argv[], char *envp[])
 void usage()
 {
 	cout
-		<< "Append " << VERSION << endl
+		<< "Append version: " << VERSION << endl
 		<< endl
 		<< "Usage" << endl
 		<< "$ append [input directory] [output filename] " << endl
